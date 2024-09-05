372057d434e4150e6a7a8f30defe0656970b63d5
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-09-05 16:58:07 -0500

    Proyecto TODO-LIST con Typescript

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { ListaTareas } from "./ListaTareas";
+
+export const TodoApp = () => {
+  const [newTask, setNewTask] = useState<string>("");
+  const [listaTareas, setlistaTareas] = useState<string[]>([]);
+
+
+
+  const handleAddTask = () => {
+    if (newTask.trim() === "") return;
+    setlistaTareas(tareaAnterior => [...tareaAnterior, newTask])
+    setNewTask("");
+  };
+
+  const handleBorrarTarea = (index: number) => {
+    setlistaTareas(tareas => tareas.filter((_, i) => i !== index))
+  }
+
+  return (
+    <div>TodoApp
+      <h1>Lista de tareas</h1>
+      <div>
+        <input type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Agregar tarea..." />
+        <button onClick={handleAddTask}>Agregar</button>
+      </div>
+      <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea} ></ListaTareas>
+    </div>
+  )
+}
